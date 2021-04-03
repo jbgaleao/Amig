672e81696bf208d62ce40d6b3707ff4cf1487310
Joao Galeao <jbgaleao@gmail.com>
2021-04-03 20:08:08 -0300

    ATUALIZAÇÃO BOOTSTRAP PARA V.3.4.1

--- Models/Fabricante.cs
+++ Models/Fabricante.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Amigurumis.Models
 {
-        [Table("Fabricante")]
+    [Table("Fabricante")]
     public class Fabricante
     {
         [Key]
